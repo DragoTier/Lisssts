c3b85fe883d9e06606e19c02aa30e7863c1f5072
Ivo Muellner <ivo.muellner@web.de>
2020-01-09 15:01:40 +0100

    Added regroupable list with sliding options

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonReorderGroup } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
+  items = [1, 2, 3];
+
+  @ViewChild(IonReorderGroup, {static: false}) reorderGroup: IonReorderGroup;
 
   constructor() {}
+
+  doReorder(ev: any){
+    ev.detail.complete();
+    //this.items = ev.detail.complete(this.items);
+  }
+
+  toggleReorderGrouo(){
+    this.reorderGroup.disabled = !this.reorderGroup.disabled;
+  }
 
 }
