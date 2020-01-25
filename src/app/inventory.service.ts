0a4e9aca767315c5b52a28bbaf8a7f4bd812e342
Ivo Muellner <ivo.muellner@web.de>
2020-01-25 17:37:56 +0100

    changed favicon + title, fixed inventory bug

--- src/app/inventory.service.ts
+++ src/app/inventory.service.ts
@@ -29,21 +29,27 @@ export class InventoryService {
 
   addToInventory(inventory: InventoryList[]) { //do not touch
     inventory = inventory.reverse();
+    //for each list in inventory compare with each list in this.inventory
     for(var t = inventory.length - 1; t >= 0; t--){
       for( var i = this.inventory.length -1; i >= 0; i--){
+        //if both contain a list with the same name
         if(inventory[t].name === this.inventory[i].name){
+          //compare each item of the list with each item of the corresponding list
           for( var j = inventory[t].items.length -1; j >= 0; j--){
             for(var k = this.inventory[i].items.length -1; k >= 0; k--){
+              //if item with same is found: add count to existing item in this.inventory
               if(inventory[t].items[j].name === this.inventory[i].items[k].name){
                 this.inventory[i].items[k].count += inventory[t].items[j].count;
                 inventory[t].items.splice(j, 1);
                 break;
               }
             }
-            inventory[t].items.forEach( (item) => {
-              this.inventory[i].items.push(item);
-            });
+            // this.inventory[i].items.push(inventory[t].items[j]);
+            
           }
+          inventory[t].items.forEach( (item) => {
+            this.inventory[i].items.push(item);
+          });
           inventory.splice(t, 1);
           break;
         }
